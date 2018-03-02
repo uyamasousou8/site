5a1e3a43225cd5158bd7dd981934c41599e59b72
nagatsuka  kaoru <uyama.sousou87@gmail.com>
2018-03-02 18:39:56 +0900

    jquery practice

---
+++ docs/js/script.js
@@ -0,0 +1,44 @@
+var TableClass = function(){
+  this.clicknumber = 1;
+};
+TableClass.prototype.addClick = function(){
+  this.clicknumber++;
+};
+TableClass.prototype.setColor = function(classtarget){
+  switch (this.clicknumber%4) {
+    case 0:
+      $("."+classtarget).css("background","#fff");
+      break;
+    case 1:
+      $("."+classtarget).css("background","#ccc");
+      break;
+    case 2:
+      $("."+classtarget).css("background","#666");
+      break;
+    case 3:
+      $("."+classtarget).css("background","#000");
+      break;
+    default:
+      $("."+classtarget).css("background","#fff");
+      break;
+  }
+};
+var tableclass = Array();
+$(function(){
+  $("td").click(function()
+  {
+    var target = "target" + $(this).parent().index() + "-" + $(this).index();
+    var index = $(this).parent().index() + $(this).index() * $(this).parent().length;
+    //alert(target);
+    $(this).addClass(target);
+    //NULLを返したらクラスを生成する。
+    if(tableclass[index] == null){
+      tableclass[index] = new TableClass();
+      tableclass[index].setColor(target);
+    }else{
+      tableclass[index].addClick();
+      tableclass[index].setColor(target);
+    }
+  }
+  );
+});
