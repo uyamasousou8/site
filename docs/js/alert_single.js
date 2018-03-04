41b4aab7cd045cbe5b68b96afcc79bb2664458a5
nagatsuka  kaoru <uyama.sousou87@gmail.com>
2018-03-04 22:29:54 +0900

    update

--- docs/js/alert_single.js
+++ docs/js/alert_single.js
@@ -2,13 +2,15 @@ document.addEventListener('DOMContentLoaded',function(){
   var bg = document.getElementById('bg');
   var add = document.getElementById('add');
   var ok = document.getElementById('ok');
-
-  //bg.style.display = 'none';
   add.addEventListener('click',function(){
-    window.alert("tes");
+    bg.style.overflow = 'auto';
+    bg.style.height = '100%';
+    bg.classList.add('open');
   });
   ok.addEventListener('click',function(){
       //bg.style.display = 'none';
       bg.classList.remove('open');
+      bg.style.overflow = 'hidden';
+      bg.style.height = '0';
   });
 });
