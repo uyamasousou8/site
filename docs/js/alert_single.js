570ae4dba07943730bd0416002dc7fc2be111dfc
nagatsuka  kaoru <uyama.sousou87@gmail.com>
2018-03-04 21:19:02 +0900

    update

--- docs/js/alert_single.js
+++ docs/js/alert_single.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded',function(){
   var bg = document.getElementById('bg');
-  var click = document.getElementById('click');
+  var add = document.getElementById('add');
   var ok = document.getElementById('ok');
 
-  bg.style.display = 'none';
-  click.addEventListener('click',function(){
-    bg.style.display = '';
+  //bg.style.display = 'none';
+  add.addEventListener('click',function(){
+    window.alert("tes");
   });
   ok.addEventListener('click',function(){
-      bg.style.display = 'none';
+      //bg.style.display = 'none';
+      bg.classList.remove('open');
   });
 });
